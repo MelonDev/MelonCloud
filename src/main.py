03928f7753513c205ab9c8dfe53025390c1700a3
MelonDev <melondev@icloud.com>
2021-11-12 02:02:44 +0700

    ทดสอบกำหนด environment

--- src/main.py
+++ src/main.py
@@ -11,5 +11,10 @@ async def index():
     return "Hello FastAPI"
 
 
+@app.get("/secret-key")
+async def secret_key():
+    return environment.SECRET_KEY
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app")
