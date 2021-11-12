20da733b3ccff61f1efb5c2dbc7fdc9248d9bc0f
MelonDev <melondev@icloud.com>
2021-11-12 15:40:41 +0700

    เพิ่มไฟล์

---
+++ src/environment/database_address.py
@@ -0,0 +1,3 @@
+database_path_list = {
+    'meloncloud': 'postgresql://aoutxcqqagfnav:5d216d8f37e0862e3ec1252b587b0acf0cc8e399491e084aea00b15c29092e81@ec2-3-227-149-67.compute-1.amazonaws.com:5432/ddbvdp836imi0u'
+}
