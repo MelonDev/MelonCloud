d643d3d17d195107e978c9f70378a5d0ed72c165
MelonDev <melondev@icloud.com>
2021-11-16 00:21:34 +0700

    ปิดการเรียกบางฟังก์ชันใน /docs

--- src/routers/page.py
+++ src/routers/page.py
@@ -4,6 +4,6 @@ from src.environment.share_environment import templates
 
 router = APIRouter()
 
-@router.get("/pwg")
+@router.get("/pwg",include_in_schema=False)
 async def pwg(request: Request):
     return templates.TemplateResponse("security/password_generator/home.html", {"request": request})
--- src/routers/playground.py
+++ src/routers/playground.py
@@ -7,20 +7,16 @@ from src.environment.database import get_db
 
 router = APIRouter()
 
-@router.get("/")
+@router.get("/",include_in_schema=False)
 async def index():
     # return "Hello FastAPI"
     return RedirectResponse(url="/docs/")
 
 
-@router.get("/database")
+@router.get("/database", include_in_schema=False)
 async def database(db: Session = Depends(get_db)):
     a = db.query(MelonDevTwitterDatabase).limit(10).all()
     b = [item.serialize for item in a]
     print("DATABASE")
     return b
 
-
-@router.get("/main", tags=["users"])
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
