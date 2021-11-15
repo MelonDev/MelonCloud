12a53a69d8af2ffcfa4e60a3c58fa031aaeb8cb5
MelonDev <melondev@icloud.com>
2021-11-15 16:09:57 +0700

    ทดสอบฐานข้อมูล

--- src/main.py
+++ src/main.py
@@ -22,9 +22,9 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-#script_dir = os.path.dirname(__file__)
-#st_abs_file_path = os.path.join(script_dir, "static/")
-#app.mount("/static", StaticFiles(directory=st_abs_file_path), name="static")
+# script_dir = os.path.dirname(__file__)
+# st_abs_file_path = os.path.join(script_dir, "static/")
+# app.mount("/static", StaticFiles(directory=st_abs_file_path), name="static")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
