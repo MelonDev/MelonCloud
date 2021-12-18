390e5f281c8fd0cc4821d7c28414f65bf715d4b1
MelonDev <melondev@icloud.com>
2021-12-19 01:44:27 +0700

    แก้บัค JWT

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi_jwt_auth.exceptions import AuthJWTException
 from starlette.responses import RedirectResponse
 from timing_asgi import TimingMiddleware, TimingClient
 from timing_asgi.integrations import StarletteScopeToName
@@ -16,21 +17,21 @@ from src.routers.security import password_generator_api as pwg_api
 from src.routers import twitter_api as twitter_api
 from src.tools.log import Colors, log
 
-'''
+
 class PrintTimings(TimingClient):
     def timing(self, metric_name, timing, tags):
         if 'time:wall' in tags:
             time = "{:.5f}".format(timing)
             log.m("DURATION: " + time, color=Colors.yellow)
-'''
+
 
 def include_router(app):
     app.include_router(page.router, prefix="", tags=["webpage"])
-    #app.include_router(twitter_api.router, prefix="/api/twitter", tags=["Twitter"])
-    #app.include_router(pwg_api.router, prefix="/api/security", tags=["Random Password Generator"])
-    #app.include_router(jwt_poc.router, prefix="/api/poc/jwt", tags=["JWT"])
-    #app.include_router(playground.router, prefix="/api/playground", tags=["playground"])
-    #app.include_router(user.router, prefix="/api/user", tags=["users"])
+    app.include_router(twitter_api.router, prefix="/api/twitter", tags=["Twitter"])
+    app.include_router(pwg_api.router, prefix="/api/security", tags=["Random Password Generator"])
+    app.include_router(jwt_poc.router, prefix="/api/poc/jwt", tags=["JWT"])
+    app.include_router(playground.router, prefix="/api/playground", tags=["playground"])
+    app.include_router(user.router, prefix="/api/user", tags=["users"])
 
 
 def configure_static(app):
@@ -56,23 +57,23 @@ def init_app():
 
 app = init_app()
 
-'''
+
 app.add_middleware(
     TimingMiddleware,
     client=PrintTimings(),
     metric_namer=StarletteScopeToName(prefix="meloncloud", starlette_app=app)
 )
-'''
 
 
-'''
+
+
 @app.exception_handler(AuthJWTException)
 def authjwt_exception_handler(request: Request, exc: AuthJWTException):
     return JSONResponse(
         status_code=exc.status_code,
         content={"detail": exc.message}
     )
-'''
+
 
 
 @app.get("/", include_in_schema=False)
