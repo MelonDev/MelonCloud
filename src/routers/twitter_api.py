51a90eef0a7657dd5446342b85765eb4be908967
MelonDev <melondev@icloud.com>
2022-01-08 11:35:01 +0700

    แก้ไขบัคการบันทึกทวีตที่มีไฟล์ภาพหรือคลิป

--- src/routers/twitter_api.py
+++ src/routers/twitter_api.py
@@ -181,11 +181,11 @@ async def manual_analyze(model: RequestDirectAnalyzeModel, db: Session):
 async def process_tweet(req, package, tweet_id, db: Session, enable_commit=True):
     if str(req.tag)[:8] == 'HASHTAG ' and is_not_retweet(package.tweet.message):
         package.tweet.event = str(req.tag)
-        if req.only_media and package.media_urls > 0:
+        if req.only_media and len(package.media_urls) > 0:
             db.add(package.tweet)
             if enable_commit:
                 db.commit()
-        elif is_circle_language(package.tweet.lang) or has_in_my_history(db, package.tweet.account):
+        elif not req.only_media and (is_circle_language(package.tweet.lang) or has_in_my_history(db, package.tweet.account)):
             db.add(package.tweet)
             if enable_commit:
                 db.commit()
