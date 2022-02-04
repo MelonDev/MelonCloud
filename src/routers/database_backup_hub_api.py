5df5cf75a74695eef3e0306081d00a133956472c
MelonDev <melondev@icloud.com>
2022-02-04 18:41:05 +0700

    แก้ไขฟังก์ชันส่งออกข้อมูล

--- src/routers/database_backup_hub_api.py
+++ src/routers/database_backup_hub_api.py
@@ -5,7 +5,7 @@ from src.database.meloncloud.meloncloud_book_database import MelonCloudBookDatab
 from src.database.meloncloud.meloncloud_book_page_database import MelonCloudBookPageDatabase
 from src.database.melondev_twitter_database import MelonDevTwitterDatabase
 from src.environment.database import get_db
-from src.tools.db_exporter import export_month_in_year
+from src.tools.db_exporter import export_month_in_year, export
 import httpx
 
 router = APIRouter()
@@ -41,9 +41,9 @@ async def melondev_twitter_database(request: Request, db: Session = Depends(get_
 
 @router.get("/meloncloud_book_database", include_in_schema=True)
 async def meloncloud_book_database(request: Request, db: Session = Depends(get_db)):
-    return export_month_in_year(db=db, session=MelonCloudBookDatabase)
+    return export(db=db, session=MelonCloudBookDatabase)
 
 
 @router.get("/meloncloud_book_page_database", include_in_schema=True)
 async def melonCloud_book_page_database(request: Request, db: Session = Depends(get_db)):
-    return export_month_in_year(db=db, session=MelonCloudBookPageDatabase)
+    return export(db=db, session=MelonCloudBookPageDatabase)
