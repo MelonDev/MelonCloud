b753aa892933234c66f96fefbe01f2b85ca43eaf
MelonDev <melondev@icloud.com>
2022-03-04 06:54:29 +0700

    เพิ่มระบบผู้คน

--- src/database/meloncloud/meloncloud_people_database.py
+++ src/database/meloncloud/meloncloud_people_database.py
@@ -32,7 +32,7 @@ class MelonCloudPeopleDatabase(Base):
 
     def __init__(self):
         self.id = uuid.uuid4()
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = current_datetime_with_timezone()
 
     def append_details(self, name: str = None, image_url: str = None, nationality: str = None, gender: str = None,
                        weight: int = None, height: int = None, year_of_birth: int = None, partner: str = None,
