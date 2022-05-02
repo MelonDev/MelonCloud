7689e37648e470e0236701cbc741cdf5ec384f01
MelonDev <melondev@icloud.com>
2022-05-03 04:01:41 +0700

    อัปเดตฐานข้อมูล

--- src/environments/database.py
+++ src/environments/database.py
@@ -3,14 +3,14 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from src.environments.database_path import database_path
 
-engine = create_engine(database_path['meloncloud'])
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+# = create_engine(database_path['meloncloud'])
+#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#Base = declarative_base()
 
 
-def get_dbs():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+#def get_dbs():
+    #db = SessionLocal()
+    #try:
+    #    yield db
+    #finally:
+    #    db.close()
